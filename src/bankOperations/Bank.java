943e8ac3a17b1edccae43702d10fde347a817cd2
Irish011 <ajaybhartiirish@gmail.com>
2023-02-26 23:18:03 +0530

    Approved Loans for Savings And Current Account

--- src/bankOperations/Bank.java
+++ src/bankOperations/Bank.java
@@ -5,7 +5,7 @@ import authentication.SignUp;
 import java.util.Random;
 import java.util.Scanner;
 
-public class Bank implements Calculate_interest {
+public class Bank implements Calculate_interest, Approve_Loan {
     private long accountNumber;
 
     public int getYears() {
@@ -87,7 +87,7 @@ public class Bank implements Calculate_interest {
     private void interest(int amount,int years){
         double rate = 5.0;
         double interestAmount = (amount * years * rate) / 100;
-        System.out.println("You Will get interest"+interestAmount+" Each years !");
+        System.out.println("You will get an interest of "+interestAmount+" each year !");
     }
 
     public void deposit(){
@@ -95,11 +95,12 @@ public class Bank implements Calculate_interest {
         System.out.println("Enter the amount you want to deposit :");
         int value = scan.nextInt();
         if(value<=0){
-            System.out.println("invalid choice");
+            System.out.println("Invalid choice");
             return;
         }
         balance+=value;
-        interest(balance,years);
+        if(isSavings)
+            interest(balance,years);
         System.out.println("Deposit successfully!");
     }
     public void withDraw(){
@@ -107,16 +108,16 @@ public class Bank implements Calculate_interest {
         System.out.println("Enter the amount you want to withdraw :");
         int value = scan.nextInt();
         if(value<=0){
-            System.out.println("invalid choice");
+            System.out.println("Invalid choice");
             return;
         }
         //always keep 2000 as balance in account you cannot withdraw more than that
         if(value > balance){
-            System.out.println("you don't have sufficient amount!");
+            System.out.println("You don't have sufficient amount!");
             return;
         }
         else if(balance-2000 < value){
-            System.out.println("You must keep 2000 rupees in your account");
+            System.out.println("You must keep atleast 2000 rupees in your account");
             return;
         }
         balance-=value;
@@ -126,8 +127,34 @@ public class Bank implements Calculate_interest {
         System.out.println("Account Info ::");
         System.out.println("Account Holder Name :"+this.getAccountHolderName());
         System.out.println("Account Holder Number :"+this.getAccountNumber());
-        System.out.println("Account Type :"+((isSavings)?"Savings":"Current"));
+        System.out.println("Account Type :"+((isSavings)?" Savings":" Current"));
         System.out.println("Account Balance :"+this.getBalance());
+    }
+
+    public void approveOnSavings(int loanAmt){
+        balance = this.getBalance();
+        if(balance >= 100000){
+            if(loanAmt>0 && loanAmt <= balance*1.5){
+                System.out.println("Your Loan is Approved!");
+            }else{
+                System.out.println("Your request has been noted. Your branch will contact you for followup!");
+            }
+        }else{
+            System.out.println("Your request has been noted. Your branch will contact you for followup!");
+        }
+    }
+
+    public void approveOnCurrent(int loanAmt){
+        balance = this.getBalance();
+        if(balance >= 500000){
+            if(loanAmt>0 && loanAmt <= balance*3){
+                System.out.println("Your Loan is Approved!");
+            }else{
+                System.out.println("Your request has been noted. Your branch will contact you for followup!");
+            }
+        }else{
+            System.out.println("Your request has been noted. Your branch will contact you for followup!");
+        }
     }
 
 
